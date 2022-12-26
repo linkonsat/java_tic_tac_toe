60ec2fdc71c388c04df64705ce2e0e123e9560ed
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2022-12-25 21:00:38 -0800

    Addef forgotten comma.

---
+++ Player.java
@@ -0,0 +1,14 @@
+class Player {
+    private char symbol = ' ';
+    public static void main(String[] args) {
+    }
+
+    public static void setSymbol(char newSymbol) {
+        symbol = newSymbol;
+    }
+
+    public static void getSymbol() {
+        return symbol
+    }
+
+}
