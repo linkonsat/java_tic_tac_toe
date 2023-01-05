71e105d985bd6d935c8ecaab8e134095b0f56feb
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2023-01-05 10:30:38 -0800

    Updated player cass with user input.

--- Player.java
+++ Player.java
@@ -1,14 +1,24 @@
+
+import java.util.*;
+
 class Player {
     private static char symbol = ' ';
     public static void main(String[] args) {
     }
 
-    public static void setSymbol(char newSymbol) {
-        symbol = newSymbol;
+    public static void setSymbol() {
+        Scanner sc = new Scanner(System.in);
+        String newSymbol = sc.next();
+        symbol = newSymbol.charAt(0);
     }
 
     public static char getSymbol() {
         return symbol;
+    }
+
+    public static void wrongSymbol() {
+        System.out.println("You have entered the same symbol as the other player.");
+        System.out.println("Please enter another symbol.");
     }
 
 }
